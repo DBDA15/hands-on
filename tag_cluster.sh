d5fd6cd68e36add9d3dae4bc928b5e84c806cd6b
tongr <tongr@users.noreply.github.com>
2015-06-15 15:46:08 +0200

    added EBS volume tags

--- tag_cluster.sh
+++ tag_cluster.sh
@@ -17,5 +17,14 @@ $HADOOP_DIR/bin/slaves.sh wget -q -O instance_id http://169.254.169.254/latest/m
 $HADOOP_DIR/bin/slaves.sh awk 1 instance_id |  cut -f 2 -d' ' | awk '{printf " %s",$0} END {print ""}' >> instance_ids
 INSTANCES=`cat instance_ids`
 
-# tag all instances
-ec2-create-tags -O $AWS_ACCESS_KEY_ID -W $AWS_SECRET_ACCESS_KEY --region $REGION $INSTANCES --tag team=$GROUP
+rm -f resource_ids
+cp instance_ids resource_ids
+
+# tag attached ebs volumes
+INSTANCE_FILTER=`echo '"'$INSTANCES'"' | awk '{$1=$1}1' OFS="\",\"" | awk '{printf "[{\"Name\":\"attachment.instance-id\",\"Values\":[%s]}]",$1}'`
+#aws ec2 describe-volumes --region $REGION --filters $INSTANCE_FILTER | grep "VolumeId" | cut -d':' -f 2 | cut -d'"' -f 2
+aws ec2 describe-volumes --region $REGION --filters $INSTANCE_FILTER | grep "VolumeId" | cut -d':' -f 2 | cut -d'"' -f 2 | awk '{printf " %s",$0} END {print ""}' >> resource_ids
+
+# tag all instances and volumes
+RESOURCES=`cat resource_ids`
+ec2-create-tags -O $AWS_ACCESS_KEY_ID -W $AWS_SECRET_ACCESS_KEY --region $REGION $RESOURCES --tag team=$GROUP
